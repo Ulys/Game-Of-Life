cf83f530882f1c2205515b07244217f8678cb2d6
unknown <Anton_Razumovskyi@EPUAKYIW0839.kyiv.epam.com>
2014-08-07 13:28:26 +0300

    debug logic

--- js/field.js
+++ js/field.js
@@ -7,128 +7,124 @@
  *@module Field
  */
 
-var FieldModule = (function () {
+var FieldModule = (function() {
 
-	/**
-	 * @constructor Field class
-	 */
-	function Field() {
-		console.log("Field ...");
+    /**
+     * @constructor Field class
+     */
+    function Field() {
+        console.log("Field ...");
 
-		this.cells = null;
-	}
-	/**
-	 * @function create field with cells
-	 * @param {HTMLElment} container
-	 */
-	Field.prototype.createField = function (container) {
-		console.log("Field create field ...");
+        this.cells = null;
+    }
+    /**
+     * @function Set cells array
+     * @param {Array[HTMLElment]} container
+     */
+    Field.prototype.setCells = function(cells) {
+        console.log("Field set cells ...");
 
-		var i, j, row, rowNum, colNum, cell,
-			allRows = container.children,
-			cellsArray = [];
+        this.cells = cells;
+        console.dir(this.cells);
+    }
 
-		for (i = 0, rowNum = allRows.length; i < rowNum; i++) {
+    /**
+     * @function run one life cicle (aka generation)
+     * @return {Array[HTMLElement]}
+     */
+    Field.prototype.runGeneration = function() {
+        console.log("Field run generation ...");
 
-			cellsArray[i] = [];
-			row = allRows[i].children;
+        var willAlive = [], // Cells which will be alive in next generation
+            that = this;
 
-			for (j = 0, colNum = row.length ; j < colNum; j++) {
+        this.cells.forEach(function(row, rowNum) {
 
-				cell = row[j];
-				cellsArray[i].push(cell);
-			}
-		}
+            var temp = willAlive.concat(row.filter(function(element, colNum) {
 
-		this.cells = cellsArray;
-	}
+                var aliveNeighbors = Field._checkNeighborhood(colNum, rowNum, that.cells);
 
-	/**
-	 * @function run one life cicle (aka generation)
-	 */
-	Field.prototype.runGeneration = function() {
-		var willAlive = [];					// Cells which will be alive in next generation
+                return Field._checkRules(element.className, aliveNeighbors);
+            }));
+            willAlive = temp;
+        });
 
-		this.cells.forEach(function(row, rowNum) {
+        return willAlive;
+    }
 
-			willAlive = willAlive.concat(row.filter(function() {
+    /**
+     * Checking neighborhood
+     * @function
+     * @param {Number} col Cell column number
+     * @param {NUmber} row Cell row number
+     * @param {Array[HTMLElement]}
+     * @return {Number} number of alive neighbors
+     */
+    Field._checkNeighborhood = function(col, row, cells) {
+        // console.log("Field check neighbors ...");
 
+        var i, j,
+            curCol = col - 1, // curCol, curY - coordiantes of left up
+            curRow = row - 1, // neightboor, from it starts checking
+            size = 3, // size of neiborhood square
+            aliveNeighbors = 0;
 
-			})
-		});
-	}
+        for (i = 0; i < size; i++, curRow++) {
 
-	/**
-	 * Checking neighborhood
-	 * @function
-	 * @param {Number} x Cell column number
-	 * @param {NUmber} y Cell row number
-	 * @param {Array[HTMLElement]}
-	 * @return {Number} number of alive neighbors
-	 */
-	Field.checkNeighborhood = function(x, y, cells) {
-		console.log("Field check neighbors ...");
+            for (j = 0; j < size; j++, curCol++) {
 
-		var i, j,
-			curX = x - 1,					// curX, curY - coordiantes of left up
-			curY = y - 1,					// neightboor, from it starts checking
-			size = 3,						// size of neiborhood square
-			aliveNeighbors = 0;
+                if (cells[curRow] && cells[curRow][curCol] &&
+                    !(curRow !== row && curCol !== col)) {
 
-		for (i = 0; i < size; i++) {
+                    cells[curRow][curCol].className === "alive" && aliveNeighbors++;
+                }
+            }
 
-			for (j = 0; j < size; j++) {
+            curCol = col - 1;
+        }
 
-				if (cells[curY] && cells[curY][curX] &&
-								curY !== y && curX !== x) {
+        return aliveNeighbors;
+    }
 
-					cells[cutY][curX].className === "alive" ?
-									aliveNeighbors++ : continue;
-				}
-			}
-		}
-	}
+    /**
+     * Number of cell neighbors to change state from dead to alive
+     * @const {Number}
+     */
+    Field.bornNumber = 3;
 
-	/**
-	 * Number of cell neighbors to change state from dead to alive
-	 * @const {Number}
-	 */
-	Field.bornNumber = 3;
+    /**
+     * Min number of neighbors of cell to still be alive
+     * @const {Number}
+     */
+    Field.minNeighborsToLive = 2;
 
-	 /**
-	 * Min number of neighbors of cell to still be alive
-	 * @const {Number}
-	 */
-	Field.minNeighborsToLive = 2;
+    /**
+     * Max number of neighbors of cell to still be alive
+     * @const {Number}
+     */
+    Field.maxNeighborsToLive = 3;
 
-	/**
-	 * Max number of neighbors of cell to still be alive
-	 * @const {Number}
-	 */
-	Field.maxNeighborsToLive = 3;
+    /**
+     * Check rules
+     * @function
+     * @param {Boolean} curState Current state of cell
+     *					true - alive
+     *					false - dead
+     * @param {Number} neighbors Number of alive neighbors
+     * @return {Boolean}
+     *				true - cell will be alive in next generation
+     *				false - cell will be dead in next generation
+     */
+    Field._checkRules = function(curState, neighbors) {
+        // console.log("Field check rules ...");
+        if (curState === "alive") {
 
-	/**
-	 * Check rules
-	 * @function
-	 * @param {Number} neighbors Number of alive neighbors
-	 * @param {Boolean} curState Current state of cell
-	 *					true - alive
-	 *					false - dead
-	 * @return {Boolean}
-	 *				true - cell will be alive in next generation
-	 *				false - cell will be dead in next generation
-	 */
-	Field.checkRules = function(neighbors, curState) {
-		console.log("Field check rules ...");
+            return Field.minNeighborsToLive === neighbors ||
+                neighbors === Field.maxNeighborsToLive;
+        }
 
-		if (curState) {
+        return neighbors === Field.bornNumber;
+    }
 
-			return Field.minNeighborsToLive <= neighbors &&
-					neighbors <= Field.maxNeighborsToLive;
-		}
-
-		return neighbors === Field.bornNumber;
-	}
-
-	return Field;
+    return Field;
 })();
