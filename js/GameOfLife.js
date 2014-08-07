b942d7a30278f75672f288bb04fd624874e7a1e8
unknown <Anton_Razumovskyi@EPUAKYIW0839.kyiv.epam.com>
2014-08-07 13:30:22 +0300

    add event handlers for field click and start button click

--- js/GameOfLife.js
+++ js/GameOfLife.js
@@ -6,37 +6,99 @@
 /**
  * @module GameOfLifeModule
  */
-var GameOfLifeModule = (function () {
+var GameOfLifeModule = (function() {
 
-	/**
-	 * GameOfLife constructor
-	 * @constructor
-	 * @param {Field} model
-	 * @param {View} view
-	 */
-	function GameOfLife(model, view) {
-		console.log("Game Of Life constructor ...");
-		this.model = model;
-		this.view = view;
-	}
-	/**
-	 * Create game
-	 * @function
-	 */
-	GameOfLife.prototype.initGame = function() {
-		console.log("Game Of Life init game ...");
+    /**
+     * @const frequency of field refresh while game is run
+     */
+    var GAME_SPEED = 250;
 
-		this.view.drawField();
-		this.model.createField(this.view.container);
+    /**
+     * GameOfLife constructor
+     * @constructor
+     * @param {Field} model
+     * @param {View} view
+     */
+    function GameOfLife(model, view) {
+        console.log("Game Of Life constructor ...");
+        this.model = model;
+        this.view = view;
+        this.gameRun = false;
+    }
 
-	}
-	/**
-	 * Add event listeners to cells and buttons
-	 * @function
-	 */
-	GameOfLife.prototype.bindEvents = function() {
+    /**
+     * Create game
+     * @function
+     */
+    GameOfLife.prototype.initGame = function() {
+        console.log("Game Of Life init game ...");
 
-	}
+        this.view.drawField();
+        this.model.setCells(this.view.cells);
+        this.bindEvents();
+    }
 
-	return GameOfLife;
+    /**
+     * Add event listeners to cells and buttons
+     * @function
+     */
+    GameOfLife.prototype.bindEvents = function() {
+
+        var that = this;
+        this.view.container.addEventListener("click", onFieldClickHandler);
+        this.view.buttons.addEventListener("click", onButtonClickHandler);
+        /**
+         * Handle click event
+         * @function
+         */
+        function onFieldClickHandler(ev) {
+            console.log("on click handler");
+
+            var source = ev.srcElement;
+            if (source.parentNode.className === "row") {
+                gameCellClick(source);
+            }
+
+            /**
+             * Invoke when game field cell click
+             * @function
+             * @param source source of click
+             */
+            function gameCellClick(source) {
+
+                if (ViewModule.aliveCell === source.className) {
+                    source.className = ViewModule.deadCell;
+                } else {
+                    source.className = ViewModule.aliveCell;
+                }
+            }
+        }
+
+        /**
+         * Inovke when button clicked
+         * @function
+         * @param source of click
+         */
+        function onButtonClickHandler(ev) {
+
+            var source = ev.srcElement;
+
+            if (source.id === "startButton") {
+                startButtonClick();
+            }
+
+            /**
+             * Invoke when start button click
+             * @function
+             * @param source source of click
+             */
+            function startButtonClick() {
+                var newGeneration = that.model.runGeneration();
+                that.view.updateField(newGeneration);
+            }
+        }
+
+    }
+
+    return GameOfLife;
 })();
